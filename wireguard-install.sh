d4e312077763e63c6229d8983972703b502db48e
Isaev Ruslan <legale.legale@gmail.com>
2024-08-02 16:24:40 +0300

    change ip to 10.11.12.1
    Signed-off-by: Isaev Ruslan <legale.legale@gmail.com>

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -92,7 +92,7 @@ function installQuestions() {
 	done
 
 	until [[ ${SERVER_WG_IPV4} =~ ^([0-9]{1,3}\.){3} ]]; do
-		read -rp "Server's WireGuard IPv4: " -e -i 10.66.66.1 SERVER_WG_IPV4
+		read -rp "Server's WireGuard IPv4: " -e -i 10.11.12.1 SERVER_WG_IPV4
 	done
 
 	until [[ ${SERVER_WG_IPV6} =~ ^([a-f0-9]{1,4}:){3,4}: ]]; do
